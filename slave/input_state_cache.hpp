d6ab607a971b646f4996151d5de5c43994d9d758
Nick <nick@mesosphere.io>
2016-04-22 19:45:24 -0700

    First pass at cache implementation, needs tests

--- slave/input_state_cache.hpp
+++ slave/input_state_cache.hpp
@@ -10,10 +10,27 @@ namespace stats {
    */
   class InputStateCache {
    public:
+    /**
+     * Returns the location of the cache for logging purposes.
+     */
     virtual const std::string& path() const = 0;
+
+    /**
+     * Returns a listing of all currently known containers, mapped to their endpoints.
+     */
     virtual container_id_map<UDPEndpoint> get_containers() = 0;
+
+    /**
+     * Adds a container (and its endpoint) to the listing.
+     * If the container_id already exists, this overrides it with a new endpoint.
+     */
     virtual void add_container(
         const mesos::ContainerID& container_id, const UDPEndpoint& endpoint) = 0;
+
+    /**
+     * Removes a container (and its endpoint) from the listing.
+     * If the container_id doesn't exist, this does nothing.
+     */
     virtual void remove_container(const mesos::ContainerID& container_id) = 0;
   };
 }
--- slave/input_state_cache_impl.cpp
+++ slave/input_state_cache_impl.cpp
@@ -1,5 +1,26 @@
 #include "input_state_cache_impl.hpp"
 
+#include <stout/fs.hpp>
+#include <stout/json.hpp>
+#include <stout/path.hpp>
+
+/*
+ * dir format:
+ * DIR/
+ *  container_id-0.json
+ *  container_id-1.json
+ *  container_id-2.json
+ *  ...
+ *
+ * file format:
+ *  { host: "some_host", port: "some_port" }
+ */
+
+namespace {
+  const std::string HOST_KEY("host");
+  const std::string PORT_KEY("port");
+}
+
 stats::InputStateCacheImpl::InputStateCacheImpl(const mesos::Parameters& parameters)
   : state_path_dir(
       params::get_str(parameters, params::STATE_PATH_DIR, params::STATE_PATH_DIR_DEFAULT)) { }
@@ -9,15 +30,88 @@ const std::string& stats::InputStateCacheImpl::path() const {
 }
 
 stats::container_id_map<stats::UDPEndpoint> stats::InputStateCacheImpl::get_containers() {
-  //TODO
-  return container_id_map<UDPEndpoint>();
+  Try<std::list<std::string>> files = os::ls(state_path_dir);
+  container_id_map<UDPEndpoint> map;
+  if (files.isError()) {
+    LOG(ERROR) << "Unable to list content of cache dir[" << state_path_dir << "]: "
+               << files.error();
+    return map;
+  }
+  for (const std::string& filename : files.get()) {
+    std::string pathstr(path::join(state_path_dir, filename));
+    Try<std::string> content = os::read(pathstr);
+    if (content.isError()) {
+      LOG(ERROR) << "Unable to read content of cache file[" << pathstr << "]: " << content.error();
+      continue;
+    }
+
+    Try<JSON::Object> content_json = JSON::parse<JSON::Object>(content.get());
+    if (content_json.isError()) {
+      LOG(ERROR) << "Unable to parse JSON in cache file[" << pathstr << "] "
+                 << "content[" << content.get() << "]: " << content_json.error();
+      continue;
+    }
+
+    Result<JSON::String> host = content_json.get().find<JSON::String>(HOST_KEY);
+    if (host.isError()) {
+      LOG(ERROR) << "Unable to parse host value in cache file[" << pathstr << "] "
+                 << "content[" << content.get() << "]: " << host.error();
+      continue;
+    } else if (host.isNone()) {
+      LOG(ERROR) << "Missing host value in cache file[" << pathstr << "] "
+                 << "content[" << content.get() << "]";
+      continue;
+    }
+
+    Result<JSON::Number> port = content_json.get().find<JSON::Number>(PORT_KEY);
+    if (port.isError()) {
+      LOG(ERROR) << "Unable to parse port value in cache file[" << pathstr << "] "
+                 << "content[" << content.get() << "]: " << port.error();
+      continue;
+    } else if (port.isNone()) {
+      LOG(ERROR) << "Missing port value in cache file[" << pathstr << "] "
+                 << "content[" << content.get() << "]";
+      continue;
+    } else if (port.get().as<long>() < 0) {
+      LOG(ERROR) << "Port value in cache file[" << pathstr << "] "
+                 << "content[" << content.get() << "] must be non-negative";
+      continue;
+    }
+
+    mesos::ContainerID container_id;
+    container_id.set_value(Path(pathstr).basename());
+    map.insert(std::make_pair(
+            container_id,
+            stats::UDPEndpoint(host.get().value, port.get().as<size_t>())));
+  }
+  return map;
 }
 
 void stats::InputStateCacheImpl::add_container(
     const mesos::ContainerID& container_id, const UDPEndpoint& endpoint) {
-  //TODO
+  if (!os::exists(state_path_dir)) {
+    LOG(INFO) << "Creating new state directory[" << state_path_dir << "]";
+    Try<Nothing> result = os::mkdir(state_path_dir);
+    if (result.isError()) {
+      LOG(ERROR) << "Failed to create directory[" << state_path_dir << "]: " << result.error();
+      return;
+    }
+  }
+
+  std::string container_path = path::join(state_path_dir, container_id.value());
+  LOG(INFO) << "Writing container file[" << container_path << "] with "
+            << "endpoint[" << endpoint.string() << "]";
+  JSON::Object json_obj;
+  json_obj.values[HOST_KEY] = endpoint.host;
+  json_obj.values[PORT_KEY] = endpoint.port;
+  Try<Nothing> result = os::write(container_path, stringify(json_obj));
 }
 
 void stats::InputStateCacheImpl::remove_container(const mesos::ContainerID& container_id) {
-  //TODO
+  std::string container_path = path::join(state_path_dir, container_id.value());
+  LOG(INFO) << "Removing container file[" << container_path << "]";
+  Try<Nothing> result = os::rm(container_path);
+  if (result.isError()) {
+    LOG(ERROR) << "Failed to remove container file[" << container_path << "]: " << result.error();
+  }
 }
