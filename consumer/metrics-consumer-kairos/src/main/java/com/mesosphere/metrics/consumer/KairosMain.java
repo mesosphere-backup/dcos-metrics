c9f0c42ebd925614f252c13b1312e1c0aa3bb362
Nick <nick@mesosphere.io>
2016-05-18 19:51:07 -0700

    Add explicit check/filter for NaN values

--- consumer/metrics-consumer-kairos/src/main/java/com/mesosphere/metrics/consumer/KairosMain.java
+++ consumer/metrics-consumer-kairos/src/main/java/com/mesosphere/metrics/consumer/KairosMain.java
@@ -5,6 +5,8 @@ import java.net.URISyntaxException;
 import org.kairosdb.client.HttpClient;
 import org.kairosdb.client.builder.Metric;
 import org.kairosdb.client.builder.MetricBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.mesosphere.metrics.consumer.common.ConsumerRunner;
 import com.mesosphere.metrics.consumer.common.MetricOutput;
@@ -15,6 +17,8 @@ import dcos.metrics.Tag;
 
 public class KairosMain {
   private static class KairosOutput implements MetricOutput {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KairosOutput.class);
+
     private final HttpClient client;
     private final boolean exitOnConnectFailure;
     private MetricBuilder metricBuilder;
@@ -32,6 +36,11 @@ public class KairosMain {
       // however, we would also need to ensure that we don't have mismatched tags within these groupings.
       // and in practice I suspect we won't see a ton of duplicate metric names between flush()es anyway
       for (Datapoint d : list.getDatapoints()) {
+        Double val = d.getValue();
+        if (Double.isNaN(val)) {
+          LOGGER.warn("Skipping value {} = NaN, it won't encode for Kairos anyway.", d.getName());
+          continue;
+        }
         Metric metric = metricBuilder.addMetric(d.getName()).addDataPoint(d.getValue());
         for (Tag t : list.getTags()) {
           metric.addTag(t.getKey(), t.getValue());
@@ -45,8 +54,9 @@ public class KairosMain {
         return;
       }
       try {
+        LOGGER.info("Writing {} metrics to KairosDB", metricBuilder.getMetrics().size());
         client.pushMetrics(metricBuilder);
-      } catch (URISyntaxException | IOException e) {
+      } catch (Throwable e) { // may throw runtime exception
         e.printStackTrace();
         if (exitOnConnectFailure) {
           System.exit(1);
