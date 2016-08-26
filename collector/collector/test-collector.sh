c21da4893d202671aeca6d15fc014fe824490b26
Nick <nick@mesosphere.io>
2016-08-26 09:32:12 -0700

    Just include the raw sample data, no gzip

--- collector/collector/test-collector.sh
+++ collector/collector/test-collector.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-gunzip sample-*.txt.gz
-
 trap "echo exiting" SIGINT
 AGENT_TEST_STATE_FILE=sample-state-response.txt \
 AGENT_TEST_SYSTEM_FILE=sample-metrics-snapshot-response.txt \
@@ -10,5 +8,3 @@ KAFKA=false \
 RECORD_OUTPUT_LOG=true \
 KAFKA_PRODUCE_COUNT=1 \
 ./start-collector.sh
-
-gzip sample-*.txt
