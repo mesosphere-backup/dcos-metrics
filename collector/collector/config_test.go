05b9aa332e2939d2b1e0949955c5801322b30e8c
Jeff Malnick <malnick@gmail.com>
2016-09-21 16:01:58 -0700

    Adding more config test coverage

--- collector/collector/config_test.go
+++ collector/collector/config_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"flag"
+	"io/ioutil"
+	"os"
 	"reflect"
 	"testing"
 )
@@ -23,5 +26,58 @@ func TestDefaultConfig(t *testing.T) {
 
 	if !testConfig.KafkaFlagEnabled {
 		t.Error("Expected Kafka flag to be enabled by default")
+	}
+}
+
+func TestSetFlags(t *testing.T) {
+	testConfig := ConfigFile{
+		ConfigPath: "/some/default/path",
+	}
+	testFS := flag.NewFlagSet("", flag.PanicOnError)
+	testConfig.setFlags(testFS)
+	testFS.Parse([]string{"-config", "/another/config/path"})
+
+	if testConfig.ConfigPath != "/another/config/path" {
+		t.Error("Expected /another/config/path for config path, got", testConfig.ConfigPath)
+	}
+}
+
+func TestLoadConfig(t *testing.T) {
+	configContents := []byte(`
+poll_agent: false
+http_profiler_enabled: false
+kafka_flag_enabled: false`)
+
+	tmpConfig, err := ioutil.TempFile("", "testConfig")
+	if err != nil {
+		panic(err)
+	}
+
+	defer os.Remove(tmpConfig.Name())
+
+	if _, err := tmpConfig.Write(configContents); err != nil {
+		panic(err)
+	}
+
+	testConfig := ConfigFile{
+		ConfigPath: tmpConfig.Name(),
+	}
+
+	loadErr := testConfig.loadConfig()
+
+	if loadErr != nil {
+		t.Error("Expected no errors loading config, got", loadErr.Error())
+	}
+
+	if testConfig.PollAgentEnabled {
+		t.Error("Expected all false config, got", testConfig.PollAgentEnabled)
+	}
+
+	if testConfig.HttpProfilerEnabled {
+		t.Error("Expected all false config, got", testConfig.HttpProfilerEnabled)
+	}
+
+	if testConfig.KafkaFlagEnabled {
+		t.Error("Expected all false config, got", testConfig.KafkaFlagEnabled)
 	}
 }
