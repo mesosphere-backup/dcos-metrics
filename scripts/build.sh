237a8277d986a7d8c50f7ae115ea2e76a2fed36f
Roger Ignazio <me@rogerignazio.com>
2016-11-30 10:19:19 -0800

    validate all *.go files have apache license header
    This commit modifies build.sh to ensure all *.go files in this repo have
    a valid Apache license header and copyright statement prior to building
    the project.

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,6 +13,25 @@ export PATH="${GOPATH}/bin:${PATH}"
 export CGO_ENABLED=0
 rm -rf $BUILD_DIR && mkdir -p $BUILD_DIR
 
+# Validate all files in repo have license header and copyright statement
+license_check=0
+for source_file in $(find . -type f -name '*.go' -not -path './examples/**' -not -path './vendor/**'); do
+    if ! grep -E 'Copyright [0-9]{4} Mesosphere, Inc.' $source_file &> /dev/null; then
+        echo "Missing copyright statement in ${source_file}"
+        license_check=$((license_check + 1))
+    fi
+    if ! grep 'Licensed under the Apache License, Version 2.0 (the "License");' $source_file &> /dev/null; then
+        echo "Missing license header in ${source_file}"
+        license_check=$((license_check + 1))
+    fi
+done
+if [[ $license_check -gt 0 ]]; then
+    echo
+    echo "ERROR: found ${license_check} cases of missing copyright statements or license headers."
+    exit $license_check
+fi
+
+# Build components
 if [[ $COMPONENT == "collector" ]]; then
     # build metrics_schema package
     pushd "${SOURCE_DIR}/schema"
