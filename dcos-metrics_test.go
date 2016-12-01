503564a4adb6859d65c8dc8a63ec8f3f33cd1b7c
Roger Ignazio <me@rogerignazio.com>
2016-11-30 16:05:38 -0800

    move config tests into config_test.go

--- dcos-metrics_test.go
+++ dcos-metrics_test.go
@@ -18,16 +18,12 @@ package main
 
 import (
 	"bytes"
-	"flag"
 	"fmt"
-	"io/ioutil"
-	"os"
 	"os/exec"
 	"testing"
 
 	yaml "gopkg.in/yaml.v2"
 
-	"github.com/Sirupsen/logrus"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -44,103 +40,6 @@ func TestMain(t *testing.T) {
 		So(stdout.String(), ShouldContainSubstring, "Revision: ")
 		So(stdout.String(), ShouldContainSubstring, "HTTP User-Agent: ")
 		So(stdout.String(), ShouldNotContainSubstring, "unset")
-	})
-}
-
-func TestDefaultConfig(t *testing.T) {
-	Convey("Ensure default configuration is set properly", t, func() {
-		testConfig := newConfig()
-
-		Convey("Default node polling period should be 15", func() {
-			So(testConfig.Collector.Node.PollPeriod, ShouldEqual, 15)
-		})
-
-		Convey("Default mesos agent polling period should be 15", func() {
-			So(testConfig.Collector.MesosAgent.PollPeriod, ShouldEqual, 15)
-		})
-
-		Convey("HTTP profiler should be enabled by default", func() {
-			So(testConfig.Collector.HTTPProfiler, ShouldBeTrue)
-		})
-
-		Convey("Default log level should be 'info'", func() {
-			So(testConfig.LogLevel, ShouldEqual, "info")
-		})
-	})
-}
-
-func TestSetFlags(t *testing.T) {
-	Convey("When command line arguments are provided", t, func() {
-		Convey("Should apply an alternate configuration path", func() {
-			testConfig := Config{
-				ConfigPath: "/some/default/path",
-			}
-			testFS := flag.NewFlagSet("", flag.PanicOnError)
-			testConfig.setFlags(testFS)
-			testFS.Parse([]string{"-config", "/another/config/path"})
-
-			So(testConfig.ConfigPath, ShouldEqual, "/another/config/path")
-		})
-
-		Convey("Should apply an alternate log level", func() {
-			testConfig := Config{
-				LogLevel: "debug",
-			}
-			testFS := flag.NewFlagSet("", flag.PanicOnError)
-			testConfig.setFlags(testFS)
-			testFS.Parse([]string{"-loglevel", "debug"})
-
-			lvl, err := logrus.ParseLevel(testConfig.LogLevel)
-			if err != nil {
-				panic(err)
-			}
-
-			So(testConfig.LogLevel, ShouldEqual, "debug")
-			So(lvl, ShouldEqual, logrus.DebugLevel)
-		})
-	})
-}
-
-func TestLoadConfig(t *testing.T) {
-	// Mock out and create the config file
-	configContents := []byte(`
----
-collector:
-  mesos_agent:
-    port: 1234
-    poll_period: 5
-    request_protocol: https
-  node:
-    poll_period: 3
-  http_profiler: false
-`)
-
-	tmpConfig, err := ioutil.TempFile("", "testConfig")
-	if err != nil {
-		panic(err)
-	}
-
-	defer os.Remove(tmpConfig.Name())
-
-	if _, err := tmpConfig.Write(configContents); err != nil {
-		panic(err)
-	}
-
-	Convey("Ensure config can be loaded from a file on disk", t, func() {
-		testConfig := Config{
-			ConfigPath: tmpConfig.Name(),
-		}
-
-		Convey("testConfig should match mocked config file", func() {
-			loadErr := testConfig.loadConfig()
-			So(loadErr, ShouldBeNil)
-
-			So(testConfig.Collector.MesosAgent.Port, ShouldEqual, 1234)
-			So(testConfig.Collector.MesosAgent.PollPeriod, ShouldEqual, 5)
-			So(testConfig.Collector.Node.PollPeriod, ShouldEqual, 3)
-			So(testConfig.Collector.HTTPProfiler, ShouldBeFalse)
-			So(testConfig.Collector.MesosAgent.RequestProtocol, ShouldEqual, "https")
-		})
 	})
 }
 
