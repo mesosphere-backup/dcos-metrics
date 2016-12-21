caba5583408763ef633d5c764fc9eb85d4ccfb94
Jeff Malnick <malnick@gmail.com>
2016-12-21 07:05:12 -0800

    Plugin producer top level configuration and producer instanciation in main

--- config.go
+++ config.go
@@ -29,6 +29,7 @@ import (
 	mesosAgent "github.com/dcos/dcos-metrics/collectors/mesos/agent"
 	"github.com/dcos/dcos-metrics/collectors/node"
 	httpProducer "github.com/dcos/dcos-metrics/producers/http"
+	pluginProducer "github.com/dcos/dcos-metrics/producers/plugin"
 	httpClient "github.com/dcos/dcos-metrics/util/http/client"
 	httpHelpers "github.com/dcos/dcos-metrics/util/http/helpers"
 
@@ -82,7 +83,8 @@ type CollectorConfig struct {
 // 'producers/kafka/kafka.go'. It is then the responsibility of the individual producers to
 // validate the configuration the user has provided and panic if necessary.
 type ProducersConfig struct {
-	HTTPProducerConfig httpProducer.Config `yaml:"http,omitempty"`
+	PluginProducerConfig pluginProducer.Config `yaml:"plugin,omitempty"`
+	HTTPProducerConfig   httpProducer.Config   `yaml:"http,omitempty"`
 	//KafkaProducerConfig  kafkaProducer.Config  `yaml:"kafka,omitempty"`
 	//StatsdProducerConfig statsdProducer.Config `yaml:"statsd,omitempty"`
 }
@@ -170,6 +172,9 @@ func newConfig() Config {
 			},
 		},
 		Producers: ProducersConfig{
+			PluginProducerConfig: pluginProducer.Config{
+				Port: 9001,
+			},
 			HTTPProducerConfig: httpProducer.Config{
 				CacheExpiry: time.Duration(120 * time.Second),
 				Port:        9000,
