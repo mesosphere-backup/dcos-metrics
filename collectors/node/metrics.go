c40a874a5388cae7c347b086ab65429073cd4ffb
Philip Norman <philip@mesosphere.io>
2017-02-23 15:34:04 -0800

    Give percent, not count, as unit for CPU metrics

--- collectors/node/metrics.go
+++ collectors/node/metrics.go
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 package node
 
 import (
@@ -22,8 +21,9 @@ import (
 
 const (
 	// Unit constants
-	COUNT = "count"
-	BYTES = "bytes"
+	COUNT   = "count"
+	BYTES   = "bytes"
+	PERCENT = "percent"
 
 	/* Metric name constants */
 	UPTIME = "uptime"
