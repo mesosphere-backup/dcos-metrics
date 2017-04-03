145b8a7a94644d3f4fa87bd78260c096daa37249
Philip Norman <philip@mesosphere.io>
2017-04-03 01:26:48 -0700

    Added standalone datadog plugin

---
+++ plugins/datadog-standalone/datadog-standalone.go
@@ -0,0 +1,218 @@
+// Copyright 2017 Mesosphere, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"net/http"
+	"strconv"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	plugin "github.com/dcos/dcos-metrics/plugins"
+	"github.com/dcos/dcos-metrics/producers"
+	"github.com/urfave/cli"
+)
+
+var (
+	pluginFlags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "datadog-key",
+			Usage: "DataDog API Key",
+		},
+	}
+	datadogConnector = func(metrics []producers.MetricsMessage, c *cli.Context) error {
+		if len(metrics) == 0 {
+			log.Error("No messages received from metrics service")
+		} else {
+			log.Info("Transmitting metrics to DataDog")
+			datadogURL := fmt.Sprintf("https://app.datadoghq.com/api/v1/series?api_key=%s", c.String("datadog-key"))
+
+			s := messagesToSeries(metrics)
+			b := new(bytes.Buffer)
+			json.NewEncoder(b).Encode(s)
+			res, err := http.Post(datadogURL, "application/json; charset=utf-8", b)
+			if err != nil {
+				log.Error(err)
+				return nil
+			}
+			defer res.Body.Close()
+			body, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				log.Fatal(err)
+				return nil
+			}
+			result := DDResult{}
+			err = json.Unmarshal(body, &result)
+			if err != nil {
+				log.Error(err)
+				return nil
+			}
+			if len(result.Errors) > 0 {
+				log.Error("Encountered errors:")
+				for _, err := range result.Errors {
+					log.Error(err)
+				}
+			}
+			if len(result.Warnings) > 0 {
+				log.Warn("Encountered warnings:")
+				for _, wrn := range result.Warnings {
+					log.Warn(wrn)
+				}
+			}
+			if result.Status == "ok" {
+				log.Info("Successfully transmitted metrics")
+			}
+		}
+
+		return nil
+	}
+)
+
+// DataPoint is a tuple of [UNIX timestamp, value]. This has to use floats
+// because the value could be non-integer.
+type DDDataPoint [2]float64
+
+// Metric represents a collection of data points that we might send or receive
+// on one single metric line.
+type DDMetric struct {
+	Metric string        `json:"metric,omitempty"`
+	Points []DDDataPoint `json:"points,omitempty"`
+	Type   *string       `json:"type,omitempty"`
+	Host   *string       `json:"host,omitempty"`
+	Tags   []string      `json:"tags,omitempty"`
+	Unit   string        `json:"unit,omitempty"`
+}
+
+// Series represents a collection of data points we get when we query for timeseries data
+type DDSeries struct {
+	Series []DDMetric `json:"series"`
+}
+
+// A successful metrics API result
+type DDResult struct {
+	Status   string   `json:"status,omitempty"`
+	Errors   []string `json:"errors,omitempty"`
+	Warnings []string `json:"warnings,omitempty"`
+}
+
+func main() {
+	log.Info("Starting Standalone DataDog DC/OS metrics plugin")
+	datadogPlugin, err := plugin.New(
+		plugin.PluginName("standalone-datadog"),
+		plugin.ExtraFlags(pluginFlags),
+		plugin.ConnectorFunc(datadogConnector))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Fatal(datadogPlugin.StartPlugin())
+}
+
+func messagesToSeries(messages []producers.MetricsMessage) *DDSeries {
+
+	series := new(DDSeries)
+
+	for _, message := range messages {
+		dimensions := &message.Dimensions
+		host := &dimensions.Hostname
+		messageTags := []string{}
+		addMessageTag := func(key, value string) {
+			if len(value) > 0 {
+				messageTags = append(messageTags, fmt.Sprintf("%s:%s", key, value))
+			}
+		}
+
+		for name, value := range dimensions.Labels {
+			addMessageTag(name, value)
+		}
+		addMessageTag("mesosId", dimensions.MesosID)
+		addMessageTag("clusterId", dimensions.ClusterID)
+		addMessageTag("containerId", dimensions.ContainerID)
+		addMessageTag("executorId", dimensions.ExecutorID)
+		addMessageTag("frameworkName", dimensions.FrameworkName)
+		addMessageTag("frameworkId", dimensions.FrameworkID)
+		addMessageTag("frameworkRole", dimensions.FrameworkRole)
+		addMessageTag("frameworkPrincipal", dimensions.FrameworkPrincipal)
+		addMessageTag("hostname", dimensions.Hostname)
+
+		for _, datapoint := range message.Datapoints {
+			t, err := parseTimestamp(datapoint.Timestamp)
+			if err != nil {
+				log.Errorf("Could not parse timestamp '%s': %v", datapoint.Timestamp, err)
+				continue
+			}
+			v, err := toFloat64(datapoint.Value)
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+
+			datapointTags := []string{}
+			addDatapointTag := func(key, value string) {
+				datapointTags = append(datapointTags, fmt.Sprintf("%s:%s", key, value))
+			}
+			for name, value := range datapoint.Tags {
+				addDatapointTag(name, value)
+			}
+
+			metric := DDMetric{
+				Metric: datapoint.Name,
+				Points: []DDDataPoint{{float64(t.Unix()), v}},
+				Tags:   append(messageTags, datapointTags...),
+				Unit:   datapoint.Unit,
+				Host:   host,
+			}
+			series.Series = append(series.Series, metric)
+		}
+	}
+
+	return series
+}
+
+func toFloat64(value interface{}) (float64, error) {
+	switch value := value.(type) {
+	case float64:
+		return value, nil
+	case float32:
+		return float64(value), nil
+	case int64:
+		return float64(value), nil
+	case int32:
+		return float64(value), nil
+	case int:
+		return float64(value), nil
+	case string:
+		return strconv.ParseFloat(value, 64)
+	}
+	return math.NaN(), fmt.Errorf("Could not convert %+v (%T) to float64", value, value)
+}
+
+func parseTimestamp(timestamp string) (*time.Time, error) {
+	if timestamp == "" {
+		return nil, errors.New("Received an empty timestamp")
+	}
+	parsed, err := time.Parse(time.RFC3339, timestamp)
+	if err != nil {
+		return nil, err
+	}
+	return &parsed, nil
+}
