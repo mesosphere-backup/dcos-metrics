c24a159fcb366d730416335d68ab28a439dfd712
Philip Norman <philipnrmn@users.noreply.github.com>
2017-04-10 16:09:27 -0700

    Add standalone datadog plugin (#89)
    * Added standalone datadog plugin
    
    * Replace inline float64 helper with plugin-level helper
    
    * Replace inline timestamp helper with plugin-level helper
    
    * Improve accuracy of the readme
    
    * Catch error from json encoder
    
    * Downgrade excessively severe loglevels
    
    * Improve detail provided in error traces
    
    * Document structs idiomatically
    
    * Warn on any status from DataDog API that is not 'ok'
    
    * Reflect lint updates in plugin definition
    
    * Test empty string, not nil
    
    * Break plugin down into units by data flow
    
    * Break datadog request into separate function
    
    * Replace else with return
    
    * Reflow logic for more thorough testing
    
    * Add tests for posting metrics to DataDog
    
    * Replace errors.New with fmt.Errorf
    
    * Ensure test server connection is closed
    
    * Remove unnecessary log import from tests
    
    * Surface an error if test fixture JSON is bad
    
    * Use inline assignment in test if statement
    
    * Fix link in README.md

---
+++ plugins/datadog-standalone/datadog-standalone.go
@@ -0,0 +1,208 @@
+// Copyright 2017 Mesosphere, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	log "github.com/Sirupsen/logrus"
+	plugin "github.com/dcos/dcos-metrics/plugins"
+	"github.com/dcos/dcos-metrics/producers"
+	"github.com/urfave/cli"
+)
+
+var (
+	pluginFlags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "datadog-key",
+			Usage: "DataDog API Key",
+		},
+	}
+)
+
+// DDDataPoint is a tuple of [UNIX timestamp, value]. This has to use floats
+// because the value could be non-integer.
+type DDDataPoint [2]float64
+
+// DDMetric represents a collection of data points that we might send or receive
+// on one single metric line.
+type DDMetric struct {
+	Metric string        `json:"metric,omitempty"`
+	Points []DDDataPoint `json:"points,omitempty"`
+	Type   *string       `json:"type,omitempty"`
+	Host   *string       `json:"host,omitempty"`
+	Tags   []string      `json:"tags,omitempty"`
+	Unit   string        `json:"unit,omitempty"`
+}
+
+// DDSeries represents a collection of data points we get when we query for timeseries data
+type DDSeries struct {
+	Series []DDMetric `json:"series"`
+}
+
+// DDResult represents the result from a DataDog API Query
+type DDResult struct {
+	Status   string   `json:"status,omitempty"`
+	Errors   []string `json:"errors,omitempty"`
+	Warnings []string `json:"warnings,omitempty"`
+}
+
+func main() {
+	log.Info("Starting Standalone DataDog DC/OS metrics plugin")
+	datadogPlugin, err := plugin.New(
+		plugin.Name("standalone-datadog"),
+		plugin.ExtraFlags(pluginFlags),
+		plugin.ConnectorFunc(datadogConnector))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Fatal(datadogPlugin.StartPlugin())
+}
+
+func datadogConnector(metrics []producers.MetricsMessage, c *cli.Context) error {
+	if len(metrics) == 0 {
+		log.Info("No messages received from metrics service")
+		return nil
+	}
+
+	log.Info("Transmitting metrics to DataDog")
+	datadogURL := fmt.Sprintf("https://app.datadoghq.com/api/v1/series?api_key=%s", c.String("datadog-key"))
+
+	result, err := postMetricsToDatadog(datadogURL, metrics)
+	if err != nil {
+		return err
+	}
+
+	if len(result.Errors) > 0 {
+		log.Error("Encountered errors:")
+		for _, err := range result.Errors {
+			log.Error(err)
+		}
+	}
+	if len(result.Warnings) > 0 {
+		log.Warn("Encountered warnings:")
+		for _, wrn := range result.Warnings {
+			log.Warn(wrn)
+		}
+	}
+	if result.Status == "ok" {
+		log.Info("Successfully transmitted metrics")
+	} else if result.Status != "" {
+		log.Warnf("Expected status to be ok, actually: %v", result.Status)
+	}
+
+	return nil
+}
+
+func postMetricsToDatadog(datadogURL string, metrics []producers.MetricsMessage) (*DDResult, error) {
+	series := messagesToSeries(metrics)
+	b := new(bytes.Buffer)
+	err := json.NewEncoder(b).Encode(series)
+	if err != nil {
+		return nil, fmt.Errorf("Could not encode metrics to JSON: %v", err)
+	}
+
+	res, err := http.Post(datadogURL, "application/json; charset=utf-8", b)
+	if err != nil {
+		return nil, fmt.Errorf("Could not post payload to DataDog: %v", err)
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Could not read response: %v", err)
+	}
+
+	result := DDResult{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func messagesToSeries(messages []producers.MetricsMessage) *DDSeries {
+	series := new(DDSeries)
+
+	for _, message := range messages {
+		dimensions := &message.Dimensions
+		host := &dimensions.Hostname
+		messageTags := []string{}
+		addMessageTag := func(key, value string) {
+			if len(value) > 0 {
+				messageTags = append(messageTags, fmt.Sprintf("%s:%s", key, value))
+			}
+		}
+
+		for name, value := range dimensions.Labels {
+			addMessageTag(name, value)
+		}
+		addMessageTag("mesosId", dimensions.MesosID)
+		addMessageTag("clusterId", dimensions.ClusterID)
+		addMessageTag("containerId", dimensions.ContainerID)
+		addMessageTag("executorId", dimensions.ExecutorID)
+		addMessageTag("frameworkName", dimensions.FrameworkName)
+		addMessageTag("frameworkId", dimensions.FrameworkID)
+		addMessageTag("frameworkRole", dimensions.FrameworkRole)
+		addMessageTag("frameworkPrincipal", dimensions.FrameworkPrincipal)
+		addMessageTag("hostname", dimensions.Hostname)
+
+		for _, datapoint := range message.Datapoints {
+			m, err := datapointToDDMetric(datapoint, messageTags, host)
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+			series.Series = append(series.Series, *m)
+		}
+	}
+
+	return series
+}
+
+func datapointToDDMetric(datapoint producers.Datapoint, messageTags []string, host *string) (*DDMetric, error) {
+	t, err := plugin.ParseDatapointTimestamp(datapoint.Timestamp)
+	if err != nil {
+		return nil, fmt.Errorf("Could not parse timestamp '%s': %v", datapoint.Timestamp, err)
+	}
+
+	v, err := plugin.DatapointValueToFloat64(datapoint.Value)
+	if err != nil {
+		return nil, err
+	}
+
+	datapointTags := []string{}
+	addDatapointTag := func(key, value string) {
+		datapointTags = append(datapointTags, fmt.Sprintf("%s:%s", key, value))
+	}
+	for name, value := range datapoint.Tags {
+		addDatapointTag(name, value)
+	}
+
+	metric := DDMetric{
+		Metric: datapoint.Name,
+		Points: []DDDataPoint{{float64(t.Unix()), v}},
+		Tags:   append(messageTags, datapointTags...),
+		Unit:   datapoint.Unit,
+		Host:   host,
+	}
+	return &metric, nil
+}
---
+++ plugins/datadog-standalone/datadog-standalone_test.go
@@ -0,0 +1,99 @@
+// Copyright 2017 Mesosphere, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/dcos/dcos-metrics/producers"
+)
+
+var (
+	nodeMetricsJSON = `{
+		"datapoints":[
+			{"name":"system.uptime","value":86799,"unit":"count","timestamp":"2017-04-08T20:37:39.259136472Z", "tags":{"foo":"bar"}},
+			{"name":"cpu.cores","value":2,"unit":"count","timestamp":"2017-04-08T20:37:39.259145164Z"},
+			{"name":"cpu.total","value":1.32,"unit":"percent","timestamp":"2017-04-08T20:37:39.259145164Z"}
+		],
+		"dimensions":{
+			"mesos_id":"378922ca-dd97-4802-a1b2-dde2f42d74ac",
+			"cluster_id":"cc477141-2c93-4b9f-bf05-ec0e163ce2bd",
+			"hostname":"192.168.65.90"
+		}
+	}`
+)
+
+func TestPostMetricsToDatadog(t *testing.T) {
+	nodeMetrics := producers.MetricsMessage{}
+	if err := json.Unmarshal([]byte(nodeMetricsJSON), &nodeMetrics); err != nil {
+		t.Fatal("Bad test fixture; could not unmarshal JSON")
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Fatalf("Could not read request body: %s", err)
+		}
+
+		var series = &DDSeries{}
+		if err := json.Unmarshal(b, &series); err != nil {
+			t.Fatal(err)
+		}
+
+		if len(series.Series) != 3 {
+			t.Fatalf("Expected 3 datapoints, got %d", len(series.Series))
+		}
+
+		uptime := series.Series[0]
+		if uptime.Points[0][0] != 1491683859 {
+			t.Fatalf("Expected unix time of 1491683859, got %v", uptime.Points[0])
+		}
+
+		expectedTags := []string{
+			// message-level tags
+			"mesosId:378922ca-dd97-4802-a1b2-dde2f42d74ac",
+			"clusterId:cc477141-2c93-4b9f-bf05-ec0e163ce2bd",
+			"hostname:192.168.65.90",
+			// metric-level tag
+			"foo:bar",
+		}
+
+		for _, expectedTag := range expectedTags {
+			tagWasFound := false
+			for _, tag := range uptime.Tags {
+				if expectedTag == tag {
+					tagWasFound = true
+					break
+				}
+			}
+			if !tagWasFound {
+				t.Fatalf("Expected to find tag %s, but it was not present", expectedTag)
+			}
+		}
+
+		if *uptime.Host != "192.168.65.90" {
+			t.Fatalf("Expected to find host 192.168.65.90, found %s", uptime.Host)
+		}
+
+	}))
+
+	defer server.Close()
+	postMetricsToDatadog(server.URL, []producers.MetricsMessage{nodeMetrics})
+}
