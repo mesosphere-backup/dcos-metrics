d5614db440f4c80ac01ba6de5b98cdd1c57b2e32
Philip Norman <philipnrmn@users.noreply.github.com>
2017-10-20 15:50:57 -0700

    Add statsd plugin (#124)
    * Add plugin for statsd
    
    * Add systemd units for statsd plugin
    
    * Add documentation for statsd plugin
    
    * Vendor etsy statsd library
    
    * Improve traces
    
    * Re-unite comment with context
    
    * Specify bitsize of 64
    
    * Test for various bitsizes of ints and floats
    
    * Catch various bitsizes of ints and floats
    
    * Remove misleading comment
    
    * Make statsd client initiation threadsafe
    
    * Treat connection state as goroutine close flag
    
    * Test that both metrics were sent

---
+++ plugins/statsd/statsd.go
@@ -0,0 +1,142 @@
+// Copyright 2017 Mesosphere, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"sync"
+
+	log "github.com/Sirupsen/logrus"
+	plugin "github.com/dcos/dcos-metrics/plugins"
+	"github.com/dcos/dcos-metrics/producers"
+	"github.com/etsy/statsd/examples/go"
+	"github.com/urfave/cli"
+)
+
+var (
+	pluginFlags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "statsd-udp-host",
+			Usage:  "The address of the statsd server",
+			EnvVar: "STATSD_UDP_HOST",
+			Value:  "127.0.0.1",
+		},
+		cli.IntFlag{
+			Name:   "statsd-udp-port",
+			Usage:  "The statsdPort of the statsd server",
+			EnvVar: "STATSD_UDP_PORT",
+			Value:  8125,
+		},
+	}
+	once         sync.Once
+	statsdClient *statsd.StatsdClient
+)
+
+func main() {
+	log.Info("Starting statsd DC/OS metrics plugin")
+
+	statsdPlugin, err := plugin.New(
+		plugin.Name("statsd"),
+		plugin.ExtraFlags(pluginFlags),
+		plugin.ConnectorFunc(statsdConnector))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Fatal(statsdPlugin.StartPlugin())
+}
+
+// statsdConnector is the method called by the plugin every time it retrieves
+// metrics from the API.
+func statsdConnector(metrics []producers.MetricsMessage, c *cli.Context) error {
+	once.Do(func() {
+		statsdHost := c.String("statsd-udp-host")
+		statsdPort := c.Int("statsd-udp-port")
+
+		log.Infof("Setting up new statsd client %s:%d", statsdHost, statsdPort)
+		statsdClient = statsd.New(statsdHost, statsdPort)
+	})
+
+	if len(metrics) == 0 {
+		log.Info("No messages received from metrics service")
+		return nil
+	}
+	for _, message := range metrics {
+		log.Debugf("Sending %d datapoints to statsd server...", len(message.Datapoints))
+		emitDatapointsOverStatsd(message.Datapoints, statsdClient)
+	}
+
+	return nil
+}
+
+// emitDatapointsOverStatsd converts datapoints to statsd format and dispatches
+// them in a batch to the statsd server
+func emitDatapointsOverStatsd(datapoints []producers.Datapoint, client *statsd.StatsdClient) error {
+	data := make(map[string]string)
+	for _, dp := range datapoints {
+		name, val, ok := convertDatapointToStatsd(dp)
+		// we silently drop metrics which could not be converted
+		if ok {
+			data[name] = val
+		}
+	}
+	client.Send(data, 1)
+	return nil
+}
+
+// convertDatapointToStatsd attempts to convert a datapoint to a statsd format
+// name + value, returning a false ok flag if the conversion failed.
+func convertDatapointToStatsd(datapoint producers.Datapoint) (string, string, bool) {
+	val, err := normalize(datapoint.Value)
+	if err != nil {
+		// This is only debug-level because we expect many NaNs in regular usage
+		log.Debugf("Metric %s failed to convert: %q", datapoint.Name, err)
+		return "", "", false
+	}
+	return datapoint.Name, fmt.Sprintf("%d|g", val), true
+}
+
+// normalize converts ints, floats and strings to rounded ints
+// It errors on other types and NaNs.
+func normalize(i interface{}) (int, error) {
+	switch v := i.(type) {
+	case int:
+		return v, nil
+	case int32:
+		return int(v), nil
+	case int64:
+		return int(v), nil
+	case float32:
+		return int(v + 0.5), nil
+	case float64:
+		// We need this check here because NaN can be a float. Casting NaN to
+		// int results in a large negative number. Even after you add 0.5.
+		if math.IsNaN(v) {
+			return -1, fmt.Errorf("Could not normalize NaN %q", v)
+		}
+		return int(v + 0.5), nil
+	case string:
+		f, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return -1, err
+		}
+		return normalize(f)
+	default:
+		return -1, fmt.Errorf("Could not normalize %q", v)
+	}
+}
---
+++ plugins/statsd/statsd_test.go
@@ -0,0 +1,154 @@
+// Copyright 2017 Mesosphere, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"math"
+	"net"
+	"testing"
+
+	"github.com/dcos/dcos-metrics/producers"
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/urfave/cli"
+)
+
+var (
+	// A message with a single `foo.bar` metric
+	fooBarMetric = producers.MetricsMessage{
+		Datapoints: []producers.Datapoint{
+			producers.Datapoint{
+				Name:      "foo.bar",
+				Value:     123,
+				Unit:      "",
+				Timestamp: "2010-01-02T00:01:02.000000003Z",
+			},
+			producers.Datapoint{
+				Name:      "foo.baz",
+				Value:     123.5,
+				Unit:      "",
+				Timestamp: "2010-01-02T00:01:02.000000003Z",
+			},
+		},
+	}
+)
+
+// mockStatsdServer listens on localhost with a random free local port,
+// returning its port, a channel for coms, and its connection
+func mockStatsdServer() (string, chan string, *net.UDPConn) {
+	msg := make(chan string)
+
+	addr := net.UDPAddr{
+		Port: 0,
+		IP:   net.ParseIP("127.0.0.1"),
+	}
+
+	conn, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		// Listen for UDP packets and relay them to the msg channel
+		for {
+			buffer := make([]byte, 1024)
+			n, _, err := conn.ReadFromUDP(buffer)
+			if err != nil {
+				// Connection was closed
+				break
+			}
+			message := string(buffer[0:n])
+			msg <- message
+		}
+	}()
+
+	_, p, _ := net.SplitHostPort(conn.LocalAddr().String())
+	return p, msg, conn
+}
+
+func TestDatapointConverter(t *testing.T) {
+	Convey("When converting a datapoint", t, func() {
+		name, val, ok := convertDatapointToStatsd(fooBarMetric.Datapoints[0])
+		Convey("It should report success", func() {
+			So(ok, ShouldBeTrue)
+		})
+		Convey("It should contain the metric name", func() {
+			So(name, ShouldEqual, "foo.bar")
+		})
+		Convey("It should pass in the metric value", func() {
+			So(val, ShouldStartWith, "123")
+		})
+		Convey("It should always be a gauge", func() {
+			So(val, ShouldEndWith, "g")
+		})
+		Convey("It should round to the nearest integer", func() {
+			_, val, _ = convertDatapointToStatsd(fooBarMetric.Datapoints[1])
+			So(val, ShouldEqual, "124|g")
+		})
+	})
+}
+
+func TestStatsdConnector(t *testing.T) {
+	// Start a UDP Server which accepts anything
+	port, msgs, conn := mockStatsdServer()
+	defer conn.Close()
+
+	// This closely follows how the plugin is constructed.
+	app := cli.NewApp()
+	app.Flags = pluginFlags
+
+	Convey("When supplying metrics to the connector", t, func() {
+		app.Action = func(c *cli.Context) {
+			// Inside app.Action, the cli context is appropriate populated
+			err := statsdConnector([]producers.MetricsMessage{fooBarMetric}, c)
+			So(err, ShouldBeNil)
+			// The order of metrics receipt is not guaranteed
+			metrics := []string{<-msgs, <-msgs}
+			So(metrics, ShouldContain, "foo.bar:123|g")
+			So(metrics, ShouldContain, "foo.baz:124|g")
+		}
+		// Pass the appropriate configuration into the plugin
+		app.Run([]string{"", "--statsd-udp-host", "127.0.0.1", "--statsd-udp-port", port})
+	})
+}
+
+func TestNormalize(t *testing.T) {
+	tests := map[interface{}]int{
+		123:             123,
+		int32(123):      123,
+		int64(123):      123,
+		float32(123.45): 123,
+		float64(123.45): 123,
+		123.54:          124,
+		"123":           123,
+		"123.45":        123,
+		"123.54":        124,
+	}
+	errors := []interface{}{
+		"NaN",
+		math.NaN(),
+	}
+	Convey("Inputs are cast and rounded", t, func() {
+		for input, expected := range tests {
+			val, err := normalize(input)
+			So(err, ShouldBeNil)
+			So(val, ShouldEqual, expected)
+		}
+		for _, input := range errors {
+			output, err := normalize(input)
+			So(output, ShouldEqual, -1)
+			So(err, ShouldNotBeNil)
+		}
+	})
+}
